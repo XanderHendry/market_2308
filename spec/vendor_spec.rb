b3d7df03a93c7e03c9b3229dff17ab32430da478
Xander Hendry <vhendry.coding@gmail.com>
2023-09-18 11:38:15 -0400

    Add #stock and #check_stock RSpec tests

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -12,4 +12,23 @@ RSpec.describe Vendor do
     expect(@vendor.name).to eq("Rocky Mountain Fresh")
     expect(@vendor.inventory).to eq({})
   end
+
+  describe '#stock' do
+    it 'will add an Item object into the Vendors inventory' do
+      @vendor.stock(@item1, 30)
+      expect(@vendor.inventory).to eq(@item1 => 30)
+      @vendor.stock(@item2, 12)
+      expect(@vendor.inventory).to eq(@item1 => 30, @item2 =. 12)
+    end
+  end
+
+  descrbe '#check_stock' do
+    it 'will return 0 if an item is not in stock' do
+    expect(@vendor.check_stock(@item1)).to eq(0)
+    end
+    it 'will return the current count of items in stock' do
+      @vendor.stock(@item1, 30)
+      expect(@vendor.check_stock(@item1)).to eq(30)
+    end
+  end
 end
