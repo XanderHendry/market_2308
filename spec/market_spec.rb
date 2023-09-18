0ba3392ea85512b89c1ef78b08a4ec2f284e04ea
Xander Hendry <vhendry.coding@gmail.com>
2023-09-18 12:56:53 -0400

    Implement #vendors_that_sell by passing test

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -41,8 +41,11 @@ RSpec.describe Market do
   
   describe '#vendors_that_sell' do
     it 'will return a list of vendors that have an item in stock' do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
       expect(@market.vendors_that_sell(@item1)).to eq([@vendor1, @vendor3])
-      expect(@market.vendors_that_sell(@item4)).to eq([@market2])
+      expect(@market.vendors_that_sell(@item4)).to eq([@vendor2])
     end
   end
 end
